24d6b0cbfab17be7eabbf717a09a0902356cca34
Tim <tim@gigalixir.com>
2024-06-24 11:32:42 -0400

    chore: remove misleading messages on sourcing

--- extra/profile.sh
+++ extra/profile.sh
@@ -2,13 +2,15 @@
 
 if [ -z "${GIG_DD_LOGGER__API_KEY}" ]; then
   if [ -z "${DATADOG_API_KEY}" ]; then
-    echo "GIG_DD_LOGGER__API_KEY nor DATADOG_API_KEY are not set. Continuing without the Gigalixir DataDog Logger."
+    #echo "GIG_DD_LOGGER__API_KEY nor DATADOG_API_KEY are not set. Continuing without the Gigalixir DataDog Logger."
+    /bin/true
   fi
 else
 
   log_shuttle_path=${log_shuttle_path:-/app/.profile.d/gigalixir_datadog_logger/log_shuttle}
   if [ ! -x $log_shuttle_path ]; then
-    echo "gigalixir_datadog_logger executable not found. Continuing without the Gigalixir DataDog Logger. '$log_shuttle_path'"
+    #echo "gigalixir_datadog_logger executable not found. Continuing without the Gigalixir DataDog Logger. '$log_shuttle_path'"
+    /bin/true
   else
     gigalixir_bin_path=${gigalixir_bin_path:-/opt/gigalixir/bin}
     cp ${gigalixir_bin_path}/log_shuttle ${gigalixir_bin_path}/log_shuttle.real
