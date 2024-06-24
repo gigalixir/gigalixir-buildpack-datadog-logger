195ff7ce371dc2b6ac7a74460a1e967414ed6ee8
Tim <tim@gigalixir.com>
2024-06-24 13:19:40 -0400

    fix: remove envvar checks

--- extra/profile.sh
+++ extra/profile.sh
@@ -1,20 +1,10 @@
 #!/usr/bin/env bash
 
-if [ -z "${GIG_DD_LOGGER__API_KEY}" ]; then
-  if [ -z "${DATADOG_API_KEY}" ]; then
-    #echo "GIG_DD_LOGGER__API_KEY nor DATADOG_API_KEY are not set. Continuing without the Gigalixir DataDog Logger."
-    /bin/true
-  fi
-else
+gigalixir_bin_path=${gigalixir_bin_path:-/opt/gigalixir/bin}
+log_shuttle_path=${log_shuttle_path:-/app/.profile.d/gigalixir_datadog_logger/log_shuttle}
 
-  log_shuttle_path=${log_shuttle_path:-/app/.profile.d/gigalixir_datadog_logger/log_shuttle}
-  if [ ! -x $log_shuttle_path ]; then
-    #echo "gigalixir_datadog_logger executable not found. Continuing without the Gigalixir DataDog Logger. '$log_shuttle_path'"
-    /bin/true
-  else
-    gigalixir_bin_path=${gigalixir_bin_path:-/opt/gigalixir/bin}
-    cp ${gigalixir_bin_path}/log_shuttle ${gigalixir_bin_path}/log_shuttle.real
-    cp $log_shuttle_path ${gigalixir_bin_path}/log_shuttle
-    chmod +x ${gigalixir_bin_path}/log_shuttle
-  fi
+# just once, move in the new log_shuttle file
+if [ ! -x ${gigalixir_bin_path}/log_shuttle.real ] && [ -x $log_shuttle_path ]; then
+  cp ${gigalixir_bin_path}/log_shuttle ${gigalixir_bin_path}/log_shuttle.real
+  cp $log_shuttle_path ${gigalixir_bin_path}/log_shuttle
 fi
