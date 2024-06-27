ff8b74ab12521724175c7d526e195270d418a67d
Tim <tim@gigalixir.com>
2024-06-27 10:56:20 -0500

    fix: log-shuttle binary path

--- test/profile_script_test.sh
+++ test/profile_script_test.sh
@@ -7,12 +7,9 @@ source $SCRIPT_DIR/.test_support.sh
 
 gigalixir_bin_path=${TEST_DIR}/gigalixir/bin
 mkdir -p ${gigalixir_bin_path}
-echo "echo 'this is the stand-in for the real log shuttle'" > ${gigalixir_bin_path}/log_shuttle
-chmod +x ${gigalixir_bin_path}/log_shuttle
+echo "echo 'this is the stand-in for the real log shuttle'" > ${gigalixir_bin_path}/log-shuttle
+chmod +x ${gigalixir_bin_path}/log-shuttle
 
-log_shuttle_path="${cache_dir}/log_shuttle"
-touch ${log_shuttle_path}
-chmod +x ${log_shuttle_path}
 
 # TESTS
 ######################
@@ -21,14 +18,15 @@ suite "profile script"
 
   test "happy path"
 
+    log_shuttle_path=$SCRIPT_DIR/../extra/log_shuttle
     output=$(log_shuttle_path=${log_shuttle_path} gigalixir_bin_path=$gigalixir_bin_path $SCRIPT_DIR/../extra/profile.sh)
 
     [ "$?" -eq "0" ]
     [ -z "$output" ]
-    [ -x "${gigalixir_bin_path}/log_shuttle.real" ]
-    grep -q "this is the stand-in for the real log shuttle" ${gigalixir_bin_path}/log_shuttle.real
-    [ -x "${gigalixir_bin_path}/log_shuttle" ]
-    diff ${gigalixir_bin_path}/log_shuttle ${log_shuttle_path} > /dev/null
+    [ -x "${gigalixir_bin_path}/log-shuttle.real" ]
+    grep -q "this is the stand-in for the real log shuttle" ${gigalixir_bin_path}/log-shuttle.real
+    [ -x "${gigalixir_bin_path}/log-shuttle" ]
+    diff ${gigalixir_bin_path}/log-shuttle ${log_shuttle_path} > /dev/null
 
 
 
