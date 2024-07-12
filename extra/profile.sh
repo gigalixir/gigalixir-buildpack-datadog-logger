9a872bb8566687aeb454babc8cf0a2e5b98009aa
Tim <tim@gigalixir.com>
2024-07-12 07:55:02 -0500

    fix: path to bash script

--- extra/profile.sh
+++ extra/profile.sh
@@ -2,7 +2,7 @@
 
 gigalixir_bin_path=${gigalixir_bin_path:-/opt/gigalixir/bin}
 log_shuttle_bin_path=${log_shuttle_bin_path:-/app/.profile.d/gigalixir_datadog_logger/log_shuttle.datadog}
-log_shuttle_sh_path=${log_shuttle_bin_path:-/app/.profile.d/gigalixir_datadog_logger/log_shuttle.sh}
+log_shuttle_sh_path=${log_shuttle_sh_path:-/app/.profile.d/gigalixir_datadog_logger/log_shuttle.sh}
 
 # just once, move in the new log_shuttle file
 if [ ! -x ${gigalixir_bin_path}/log-shuttle.real ] && \
