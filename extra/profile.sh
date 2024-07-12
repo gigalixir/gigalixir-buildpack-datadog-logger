b635d84b66aecbfb8d91839cd2556ead2c8fdb70
Tim <tim@gigalixir.com>
2024-07-12 07:59:44 -0500

    fix: typo

--- extra/profile.sh
+++ extra/profile.sh
@@ -7,7 +7,7 @@ log_shuttle_sh_path=${log_shuttle_sh_path:-/app/.profile.d/gigalixir_datadog_log
 # just once, move in the new log_shuttle file
 if [ ! -x ${gigalixir_bin_path}/log-shuttle.real ] && \
    [ -x ${log_shuttle_bin_path} ] && \
-   [ -x %{log_shuttle_sh_path} ]; then
+   [ -x ${log_shuttle_sh_path} ]; then
 
   cp ${gigalixir_bin_path}/log-shuttle ${gigalixir_bin_path}/log-shuttle.real
   cp $log_shuttle_bin_path ${gigalixir_bin_path}/log-shuttle.datadog
